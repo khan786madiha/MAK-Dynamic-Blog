173d085a41cdeb085e86c51183093b19b8f6b5de
Madiha Khan <mahaalikhanalizafarkhan@gmail.com>
2024-12-29 22:43:14 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,63 +1,4 @@
 
-
-// "use client"
-// import { FaArrowRight } from "react-icons/fa";
-// import Link from 'next/link'
-// import React, { useEffect, useState } from 'react'
-
-// function Navbar() {
-//   const [currentDateTime, setCurrentDateTime] = useState<string>('');
-
-//   useEffect(() => {
-//     const updateDateTime = () => {
-//       const now = new Date();
-//       const options: Intl.DateTimeFormatOptions = {
-//         weekday: 'long',
-//         year: 'numeric',
-//         month: 'long',
-//         minute: '2-digit',
-//         second: '2-digit',
-//         hour12: false,
-//       };
-//       setCurrentDateTime(
-//         now.toLocaleDateString("en-US", options) + " " + now.toLocaleTimeString("en-US", { hour12: false })
-//       );
-//     };
-
-//     updateDateTime();
-//     const intervalId = setInterval(updateDateTime, 1000);
-//     return () => clearInterval(intervalId);
-//   }, []);
-
-//   return (
-//     <header className='text-gray-400 body-font shadow-lg'>
-//       <div className="container mx-auto flex flex-wrap p-2 flex-col md:flex-row items-center">
-//         <div className='flex-1 text-black'>
-//           <span>{currentDateTime}</span>
-//         </div>
-
-//         <nav className='md:ml-auto flex flex-wrap items-center text-black justify-center'>
-//           <Link href={"http://localhost:3000/"} className='mr-5 hover:text-red-700 transition-colors duration-300 transform hover:scale-105'>
-//             HOME
-//           </Link>
-
-//           <Link href={"http://localhost:3000/About"} className='mr-5 hover:text-red-700 transition-colors duration-300 transform hover:scale-105'>ABOUT</Link>
-
-//           <Link href={"http://localhost:3000/"} className='mr-5 hover:text-red-700 transition-colors duration-300 transform hover:scale-105'>BLOG</Link>
-
-//           <Link href={"http://localhost:3000/Contact"} className='mr-5 hover:text-red-700 transition-colors duration-300 transform hover:scale-105'>CONTACT</Link>
-//         </nav>
-
-//         <button className='inline-flex items-center bg-blue-700 border-0 py-1 px-3 focus:outline-none hover:bg-red-700 rounded text-white mt-4 md:mt-0 transition-colors duration-300'>
-//           Github <FaArrowRight />
-//         </button>
-//       </div>
-//     </header>
-//   )
-// }
-
-// export default Navbar;
-
 "use client"
 import { FaArrowRight } from "react-icons/fa";
 import Link from 'next/link';
@@ -115,14 +56,14 @@ function Navbar() {
           } md:flex md:ml-auto flex flex-col md:flex-row items-center text-white justify-center mt-4 md:mt-0`}
         >
           {[
-            { href: "/", label: "HOME" },
-{ href: "/About", label: "ABOUT" },
-{ href: "/Blog", label: "BLOG" },
-{ href: "/Contact", label: "CONTACT" },
-//           { href: "/", label: "HOME" },
+// { href: "/", label: "HOME" },
 // { href: "/About", label: "ABOUT" },
-// { href: "/", label: "BLOG" },  // Updated BLOG link to point to home page
+// { href: "/Blog"            , label: "BLOG" },
 // { href: "/Contact", label: "CONTACT" },
+          { href: "/", label: "HOME" },
+{ href: "/About", label: "ABOUT" },
+{ href: "/", label: "BLOG" },  // Updated BLOG link to point to home page
+{ href: "/Contact", label: "CONTACT" },
           ].map((link, index) => (
             <Link
               key={index}
